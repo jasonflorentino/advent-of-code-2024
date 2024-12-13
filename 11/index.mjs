999d5f44374a78464a691a0bc62c945803692051
Jason Florentino <jason.florentino@relayfi.com>
2024-12-13 00:08:48 -0500

    11

---
+++ 11/index.mjs
@@ -0,0 +1,77 @@
+import fs from 'node:fs'
+
+function loadInput(name) {
+    const parts = fs.readFileSync(name).toString().trim().split(' '); 
+    const data = parts.map((p) => Number(p.trim()));
+    return data;
+}
+
+function isSplittable(n) {
+    return String(n).length % 2 === 0;
+}
+
+function split(n) {
+    const strung = String(n);
+    const half = strung.length >> 1;
+    return [strung.slice(0, half), strung.slice(half)].map(Number);
+}
+
+function nextGen(n) {
+    switch (true) {
+        case n === 0:
+            return [1];
+        case isSplittable(n):
+            return split(n);
+        default:
+            return [n * 2024]; 
+    }
+}
+
+function getNextGen(arr) {
+    return arr.flatMap(nextGen);
+}
+
+function generate(arr, n) {
+    let gen = arr;
+    while (n-- > 0) gen = getNextGen(gen);
+    return gen;
+}
+
+function update(n, c, rec) {
+    const res = nextGen(Number(n));
+    for (const n2 of res) {
+        if (rec[n2]) {
+            rec[n2] += c;
+        } else {
+            rec[n2] = c;
+        }
+    }
+}
+
+function toStoneRecord(stones) {
+    return stones.reduce((rec, s) => {
+        if (rec[s]) rec[s]++;
+        else rec[s] = 1;
+        return rec;
+    }, {});
+}
+
+async function main(name) {
+    const data = loadInput(name)
+    
+    const nums = generate(data, 25); // 191690
+    console.log(nums.length);
+    
+    let generation = 0;
+    let stones = toStoneRecord(data); 
+    while (generation++ < 75) {
+        const newGen = {};
+        Object.entries(stones).forEach(([n, c]) => update(n, c, newGen));
+        stones = newGen;
+    } 
+    const total = Object.values(stones).reduce((t, n) => t + n, 0);
+    console.log(total);
+    // 228651922369703
+}
+
+main(process.argv[2])
